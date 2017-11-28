a4531db4a944f8fffb4ae0d896b49e5c1e0d5c23
engineersworkbook <shivamkalra2013@gmail.com>
2017-11-28 19:28:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/engineers_workbook/fetchinggithub/Displaysearch_activity.java
@@ -0,0 +1,15 @@
+package com.example.engineers_workbook.fetchinggithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Displaysearch_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaysearch_activity);
+    }
+
+
+}
